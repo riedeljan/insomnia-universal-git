fe80e98adfca515d5c9f23e6e4cf173e76731b2f
Ernest Mallett <ernest.e.mallett2@gmail.com>
2021-08-15 23:50:52 -0500

    Updated providers and moved them into their own components, also created the providerselector component.

---
+++ src/gitProviders/github.tsx
@@ -0,0 +1,217 @@
+import * as React from "react";
+import axios from 'axios';
+
+export class GitHub extends React.Component<any, any>{
+
+    constructor(props) {
+        super(props);
+        console.log(this.context);
+        this.handleChange = this.handleChange.bind(this);
+        this.createNewBranch = this.createNewBranch.bind(this);
+    }
+
+    async componentDidMount() {
+        await this.loadBranches();
+    }
+
+    private handleChange(event) {
+        const { target: { name, value } } = event;
+        this.props.onElementChange(name,value);
+        console.debug("Update state property: ", name, value);
+    }
+
+    private async loadBranches() {
+        const branches: Array<any> = await this.fetchBranches();
+        console.log("Branches:"+branches);
+        const branchOptions = branches.map((b) => {
+            let rObj = {};
+            rObj['value'] = b;
+            rObj['label'] = b;
+            return rObj;
+        });
+        this.props.onElementChange('branch',this.props.context.branch ? this.props.context.branch : branches[0]);
+        this.props.onElementChange('branchOptions',branchOptions);
+    }
+
+    const // @ts-ignore
+    createNewBranch = async () =>{
+
+        try {
+            var branchName = await this.props.app.prompt(
+                'Set new branch name:', {
+                    label: 'Branch name',
+                    defaultValue: 'develop',
+                    submitName: 'Submit',
+                    cancelable: true,
+                }
+            );
+
+            await this.createRemoteBranchFromCurrent(branchName);
+
+            this.props.onElementChange('branch',branchName);
+
+            await this.loadBranches();
+            await this.props.app.alert('Success!', `Created new branch "${branchName}".`);
+
+        } catch (e) {
+            console.error(e);
+            await this.props.app.alert('Error!', 'Something went wrong. Does the branch exist already?.');
+            throw 'Creating a new branch via API failed.';
+        }
+    }
+
+    private flexContainerStyle = {
+        'display': 'flex'
+    }
+
+    private newBranchButtonStyle = {
+        'display': 'flex',
+        'flex-direction': 'row',
+        'flex-basis': '50%'
+    }
+
+    private flexChild ={
+        'flex':  '1 1 auto'
+    }
+
+    render() {
+        return (
+            <form className="pad">
+        <div className="form-control form-control--outlined">
+            <label>
+                Organization:
+                <input name="organization" type="text" placeholder="" value={this.props.context.organization}
+                       onChange={this.handleChange}/>
+                <input name="baseUrl" type="hidden" placeholder="https://api.github.com" value="https://api.github.com" onChange={this.handleChange} />
+                <input name="provider" type="hidden" value="github" onChange={this.handleChange} />
+            </label>
+        <label>
+            Access Token:
+            <input name="token" type="text" placeholder="accessToken123" value={this.props.context.token} onChange={this.handleChange} />
+        </label>
+        <label>
+        Project Name:
+        <input name="projectId" type="text" placeholder="23" value={String(this.props.context.projectId)} onChange={this.handleChange} />
+        </label>
+        <label>
+        Workspace File Name:
+            <input name="configFileName" type="text" placeholder="config.json" value={this.props.context.configFileName} onChange={this.handleChange} />
+        </label>
+        <label>
+            Branch:
+            <div style={this.flexContainerStyle}>
+                <select name="branch" style={this.flexChild} value={this.props.context.branch} onChange={this.handleChange}>
+                    {this.props.context.branchOptions.map((branch) => (<option key={branch.value} value={branch.value}>{branch.label}</option>))}
+                        </select>
+                <div style={this.flexChild}>
+                    <button type="button" style={this.newBranchButtonStyle} onClick={this.createNewBranch}>New Branch</button>
+                </div>
+            </div>
+                </label>
+                </div>
+            </form>
+    );
+    }
+
+    authenticate() {
+        return axios.create({
+            baseURL: `${this.props.context.baseUrl}`,
+            timeout: 1000,
+            headers: { Authorization: `Bearer ${this.props.context.token}` },
+            responseType: 'json'
+        });
+    }
+
+    private async initRemoteConfigFile() {
+        try {
+            await this.authenticate().post(
+                `${this.props.context.baseUrl}/repos/${this.props.context.organization}/${this.props.context.projectId}/contents/${this.props.context.configFileName}`,
+                {
+                    "branch": this.props.context.branch,
+                    "content": "IA==",
+                    "message": `Init new config file ${this.props.context.configFileName}`
+                }
+            );
+        } catch(e) {
+            console.error(e.response);
+            throw 'Creating a new file via Github API failed.'
+        }
+    }
+
+    async createRemoteBranchFromCurrent(branchName) {
+        try {
+            await this.authenticate().post(`${this.props.context.baseUrl}/repos/${this.props.context.organization}/${this.props.context.projectId}/git/refs`,{"ref":"refs/heads/"+branchName,"sha":this.props.context.branch});
+        } catch(e) {
+            console.error(e.response);
+            throw 'Creating a new branch via Github API failed.'
+        }
+    }
+
+    async fetchBranches() {
+        if (!this.props?.context.baseUrl || !this.props?.context.projectId || !this.props?.context.token) {
+            return [];
+        }
+        try {
+            const response = await this.authenticate().get(`${this.props.context.baseUrl}/repos/${this.props.context.organization}/${this.props.context.projectId}/git/refs/heads`,);
+            return response.data.map((o) => {
+                let heads = o.ref.split("/");
+                return heads[heads.length-1];
+            });
+
+        } catch(e) {
+            console.error(e);
+            throw 'Fetching the projects branches via Github API failed.'
+        }
+    }
+
+    async pullWorkspace() {
+        try {
+            const getFileResponse = await this.authenticate().get(`${this.props.context.baseUrl}/repos/${this.props.context.organization}/${this.props.context.projectId}/contents/${this.props.context.configFileName}`);
+            const response = await this.authenticate().get(getFileResponse.data.download_url);
+            return(response.data);
+        } catch (e) {
+            console.error(e);
+            throw 'Fetching the workspace via Github API failed.'
+        }
+    }
+
+    findObjectByKey(array, key, value) {
+        for (var i = 0; i < array.length; i++) {
+            if (array[i][key] === value) {
+                return array[i];
+            }
+        }
+        return null;
+    }
+
+    async pushWorkspace(content, messageCommit) {
+        try {
+            const buff = Buffer.from(content, 'utf-8');
+            const contentBase64 = buff.toString('base64');
+
+            let data = {
+                "branch": this.props.context.branch,
+                "message": messageCommit,
+                "content":contentBase64,
+                sha: null
+            };
+
+            const refs = await this.authenticate().get(`${this.props.context.baseUrl}/repos/${this.props.context.organization}/${this.props.context.projectId}/git/refs/heads`);
+            const info = this.findObjectByKey(refs.data, "ref", `refs/heads/${this.props.context.branch}`);
+            const tree = await this.authenticate().get(`${this.props.context.baseUrl}/repos/${this.props.context.organization}/${this.props.context.projectId}/git/trees/${info.object.sha}`);
+            const fileTreeInfo = this.findObjectByKey(tree.data.tree,"path",this.props.context.configFileName);
+
+            if(fileTreeInfo != null){
+                data.sha=fileTreeInfo.sha;
+            }
+
+            await this.authenticate().put(
+                `${this.props.context.baseUrl}/repos/${this.props.context.organization}/${this.props.context.projectId}/contents/${this.props.context.configFileName}`,
+                data,
+            );
+        } catch(e) {
+            console.error("response:", e.response);
+            throw 'Pushing the workspace via Github API failed.'
+        }
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,185 +1,12 @@
-import { Github } from './gitProviders/github';
-import { Gitlab } from './gitProviders/gitlab';
 import { UserConfig } from './interfaces/UserConfig';
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-
-async function loadConfig(context): Promise<UserConfig | null> {
-    const storedConfig = await context.store.getItem('gitlab-sync:config');
-    try {
-        return JSON.parse(storedConfig);
-    } catch(e) {
-        return null;
-    }
-}
-
-async function storeConfig(context, userConfig: UserConfig) {
-    await context.store.setItem('gitlab-sync:config', JSON.stringify(userConfig));
-}
-
-function getProvider(config){
-    return (config.provider == "github")?new Github(config):new Gitlab(config);
-}
-
-class ConfigForm extends React.Component<any, any> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            'baseUrl': "",
-            'token': "",
-            'projectId': null,
-            'configFileName': "",
-            'branch': "",
-            'branchOptions': []
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.createNewBranch = this.createNewBranch.bind(this);
-    }
-
-    async componentDidMount() {
-        const config: UserConfig = await loadConfig(this.props.context);
-        this.setState(config);
-        await this.loadBranches();
-    }
-
-    private handleChange(event) {
-        const { target: { name, value } } = event;
-        console.debug("Update state property: ", name, value);
-        this.setState({[name]: value});
-    }
-    
-    private async handleSubmit(event) {
-        try {
-            storeConfig(this.props.context, this.state as UserConfig);
-            await this.props.context.app.alert('Success!', 'To change your configuration, just start the setup again.');
-        } catch(e) {
-            console.error(e);
-            await this.props.context.app.alert('Error!', 'Something went wrong. Please start the setup again.');
-        }
-        event.preventDefault();
-    }
-
-    private async loadBranches() {
-
-        const provider = getProvider(this.state);
-
-        const branches: Array<any> = await provider.fetchBranches();
-        console.log(branches);
-        const branchOptions = branches.map((b) => {
-            let rObj = {};
-            rObj['value'] = b;
-            rObj['label'] = b;
-            return rObj;
-        });
-
-        this.setState({
-            'branch': this.state.branch ? this.state.branch : branches[0],
-            'branchOptions': branchOptions
-        });
-    }
-
-    private async createNewBranch() {
-          try {
-            var branchName = await this.props.context.app.prompt(
-                'Set new branch name:', {
-                label: 'Branch name',
-                defaultValue: 'develop',
-                submitName: 'Submit',
-                cancelable: true,
-                }
-            );
-
-              const provider = getProvider(this.state);
-              await provider.createRemoteBranchFromCurrent(branchName);
-            
-            this.setState({
-                'branch': branchName
-            });
-            this.loadBranches();
-            await this.props.context.app.alert('Success!', `Created new branch "${branchName}".`);
-
-          } catch (e) { 
-              console.error(e);
-              await this.props.context.app.alert('Error!', 'Something went wrong. Does the branch exist already?.');
-              throw 'Creating a new branch via API failed.';
-        }
-      }
-
-    private flexContainerStyle = {
-        'display': 'flex'
-    }
-
-    private newBranchButtonStyle = {
-        'display': 'flex',
-        'flex-direction': 'row',
-        'flex-basis': '50%'
-    }
-
-    private submitButtonStyle = {
-        'display': 'flex',
-        'flex-direction': 'row-reverse',
-        'flex-basis': '50%'
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} className="pad">
-                <div className="form-control form-control--outlined">
-                    <label>
-                        Provider:
-                        <select name="provider" value={this.state.provider} onChange={this.handleChange}>
-                            <option value="github">GitHub</option>
-                            <option value="gitlab">GitLab</option>
-                        </select>
-                    </label>
-                    <label>
-                        BaseURL:
-                        <input name="baseUrl" type="text" placeholder="https://your.gitlab-instance.com" value={this.state.baseUrl} onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Access Token:
-                        <input name="token" type="text" placeholder="accessToken123" value={this.state.token} onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        {(this.state.provider == "github")?"Project Name":"Project ID"}:
-                        <input name="projectId" type="text" placeholder="23" value={String(this.state.projectId)} onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Workspace File Name:
-                        <input name="configFileName" type="text" placeholder="config.json" value={this.state.configFileName} onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Branch:
-                        <select name="branch" value={this.state.branch} onChange={this.handleChange}>
-                            {this.state.branchOptions.map((branch) => (<option key={branch.value} value={branch.value}>{branch.label}</option>))}
-                        </select>
-                    </label>
-                    {this.state.provider == "github" &&
-                        <label>
-                            Organization:
-                            <input name="organization" type="text" placeholder="" value={this.state.organization}
-                                   onChange={this.handleChange}/>
-                        </label>
-                    }
-                </div>
-                <div style={this.flexContainerStyle}>
-                    <div className="margin-top" style={this.newBranchButtonStyle}>
-                        <button type="button" onClick={this.createNewBranch}>New Branch</button>
-                    </div>
-                    <div className="margin-top" style={this.submitButtonStyle}>
-                        <button type="submit">Submit</button>
-                    </div>
-                </div>
-            </form>
-        );
-      }
-}
+import * as Helpers from './helpers'
+import {ProviderSelector} from "./providerselector";
 
 async function pushWorkspace(context, models) {
     try {
-        const config: UserConfig = await loadConfig(context);
+        const config: UserConfig = await Helpers.loadConfig(context);
 
         var commitMessage = await context.app.prompt(
             'GitLab - Push Workspace - Commit Message', {
@@ -196,7 +23,7 @@ async function pushWorkspace(context, models) {
             workspace: models.workspace
         });
 
-        const provider = getProvider(config);
+        const provider = Helpers.getProvider(config);
 
         // parse, format, stringify again. Ugly but necessary because of Insomnia API design
         await provider.pushWorkspace(
@@ -217,8 +44,8 @@ async function pushWorkspace(context, models) {
 
 async function pullWorkspace(context) {
     try {
-        const config: UserConfig = await loadConfig(context);
-        const provider = getProvider(config);
+        const config: UserConfig = await Helpers.loadConfig(context);
+        const provider = Helpers.getProvider(config);
 
         const workspace = await provider.pullWorkspace();
         await context.data.import.raw(JSON.stringify(workspace));
@@ -235,7 +62,8 @@ const workspaceActions = [
         icon: 'fa-cogs',
         action(context, models) {
             const root = document.createElement('div');
-            ReactDom.render(<ConfigForm context={context}/>, root);
+            ReactDom.render(<ProviderSelector context={context}/>,root);
+           // ReactDom.render(<GitLab context={context}/>, root);
 
             context.app.dialog('Git Setup', root, {
                 skinny: true,
@@ -257,7 +85,6 @@ const workspaceActions = [
         icon: 'fa-arrow-up',
         action: async(context, models) => {
             await pushWorkspace(context, models);
-
         },
     }
 ];
